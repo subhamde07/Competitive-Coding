217bcc3d11bb19b0c3962061ea43563bbe748532
Subham De <78950020+subhamde07@users.noreply.github.com>
2024-04-05 01:51:44 +0530

    Time: 15 ms (6.41%) | Memory: 9.7 MB (18.70%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> mp1,mp2;
+        for(int i=0;i<s.size();i++){
+            mp1[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mp2[t[i]]++;
+        }
+        return mp1==mp2;
+    }
+};
