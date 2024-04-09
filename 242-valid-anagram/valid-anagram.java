73cd51091638853f823783f69bfe17b5d71b5ae3
Subham De <78950020+subhamde07@users.noreply.github.com>
2024-04-10 01:25:11 +0530

    Time: 16 ms (24.30%) | Memory: 44.8 MB (17.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,26 +1,21 @@
 
 class Solution {
+    public HashMap<Character, Integer> makeFreqMap(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0 ; i<str.length() ; i++){
+            Character ch = str.charAt(i);
+            if(!map.containsKey(ch)){
+                map.put(ch, 1);
+            } else{
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+        return map;
+    }
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        
-        if(s.length() != t.length()){
-            return false;
-        }
-        
-        int l = s.length();
-        for(int i = 0 ; i<l ; i++){
-            if(!map1.containsKey(s.charAt(i))){
-                map1.put(s.charAt(i), 1);
-            } else{
-                map1.put(s.charAt(i), map1.get(s.charAt(i))+1);
-            }
-            if(!map2.containsKey(t.charAt(i))){
-                map2.put(t.charAt(i), 1);
-            } else{
-                map2.put(t.charAt(i), map2.get(t.charAt(i))+1);
-            }
-        }
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> map1 = makeFreqMap(s);
+        HashMap<Character, Integer> map2 = makeFreqMap(t);
         return map1.equals(map2);
     }
 }
