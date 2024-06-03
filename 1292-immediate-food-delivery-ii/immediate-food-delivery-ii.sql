8e555df92aa5469f4f71dee111bc9ab66ba22660
Subham De <78950020+subhamde07@users.noreply.github.com>
2024-06-03 12:59:31 +0530

    Time: 1063 ms (73.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,10 @@
+
+SELECT 
+    ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0))*100 / COUNT(order_date), 2) AS                 immediate_percentage
+FROM Delivery
+WHERE(customer_id, order_date) 
+IN(
+    SELECT customer_id, MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id
+)
