ab64e63087155f91eec57419685e49712bd7299b
Subham De <78950020+subhamde07@users.noreply.github.com>
2024-07-18 23:43:11 +0530

    Time: 1 ms (99.61%) | Memory: 43.9 MB (54.24%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;  // Position to write the compressed character(s)
+        int i = 0;      // Position to read the original character(s)
+
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            // Count occurrences of the current character
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+            // Write the character
+            chars[index++] = currentChar;
+
+            // Write the count if it's greater than 1
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
