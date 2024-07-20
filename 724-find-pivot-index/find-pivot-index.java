3eecbc50b66a9577279c1defc827e0ef981d3810
Subham De <78950020+subhamde07@users.noreply.github.com>
2024-07-20 23:53:36 +0530

    Time: 9 ms (11.48%) | Memory: 42.8 MB (99.92%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,8 +1,9 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-
+        // Best RunTime
+        /*
         if (nums.length == 0) return -1;
-        
+
         int leftSum = 0, rightSum = 0;
         for (int num : nums)
             rightSum += num;
@@ -14,5 +15,27 @@ class Solution {
             leftSum += nums[i];
         }
         return -1;
+        */
+
+        // Other Approach For Best Memory Management
+
+        int l = nums.length;
+        int[] leftPrefix = new int[l];
+        int[] rightPrefix = new int[l];
+        leftPrefix[0] = nums[0];
+        rightPrefix[l-1] = nums[l-1];
+
+        for(int i = 1; i < l; i++) {
+            leftPrefix[i] = leftPrefix[i-1] + nums[i];
+            rightPrefix[l-1-i] = rightPrefix[l-i] + nums[l-1-i];
+        }
+        int i = 0;
+        while(leftPrefix[i] != rightPrefix[i]) {
+            i++;
+            if (i == l) return -1;
+        }
+
+        System.gc();
+        return i;
     }
 }
