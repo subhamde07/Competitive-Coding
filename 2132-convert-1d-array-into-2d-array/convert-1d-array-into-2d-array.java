c248420979039d5daf07d0c60bdb1a3a43909c99
Subham De <78950020+subhamde07@users.noreply.github.com>
2024-09-01 16:45:26 +0530

    Time: 2 ms (100.00%) | Memory: 55.8 MB (11.21%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -4,9 +4,9 @@ class Solution {
             return new int[0][0];
         }
 
-        int[][] ans = new int[m][n];
-        for(int i=0 ; i < m*n ; i++){
-            ans[i / n][i % n] = original[i];
+        int[][] ans = new int[m][];
+        for(int i=0 ; i<m ; i++){
+            ans[i] = Arrays.copyOfRange(original, i*n, i*n+n);
         }
 
         return ans;
